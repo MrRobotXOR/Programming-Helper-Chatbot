2bb41dde020b474603f93f742de78a160f78d114
MrRobotXOR <shubhamkurhade794@gmail.com>
2026-09-07 22:14:32 +0530

    CORS update and deployment setup

--- server/server.js
+++ server/server.js
@@ -15,7 +15,23 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://programming-helper.vercel.app",
+];
+
+app.use(
+  cors({
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/chat", chatRoutes);
 
