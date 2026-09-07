3a706b3f8307276cfaf16f0fe49aae66eb2f9672
MrRobotXOR <shubhamkurhade794@gmail.com>
2026-09-08 00:24:33 +0530

    Add root gitignore for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Har request me token bhejo
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
